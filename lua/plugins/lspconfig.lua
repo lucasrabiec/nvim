cb7d26f233dfdc5af083317e05b086eb511a84e8
Lukasz Rabiec <lukaszrabiec@gmail.com>
2026-04-16 00:15:44 +0200

    feat: enable file watcher for all lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,17 +1,28 @@
 return {
   "neovim/nvim-lspconfig",
-  -- opts = {
-  --   servers = {
-  --     eslint = {
-  --       on_attach = function(_, bufnr)
-  --         -- vim.api.nvim_create_autocmd({ "BufWritePre", "BufLeave", "WinLeave", "FocusLost" }, {
-  --         vim.api.nvim_create_autocmd({ "BufWritePre", "WinLeave", "FocusLost" }, {
-  --           buffer = bufnr,
-  --           command = "EslintFixAll",
-  --         })
-  --       end,
-  --       root_dir = vim.fs.dirname(vim.fs.find(".git", { path = startpath, upward = true })[1]),
-  --     },
-  --   },
-  -- },
+  opts = {
+    servers = {
+      ["*"] = {
+        capabilities = {
+          workspace = {
+            didChangeWatchedFiles = {
+              dynamicRegistration = true,
+            },
+          },
+        },
+      },
+    },
+    --   servers = {
+    --     eslint = {
+    --       on_attach = function(_, bufnr)
+    --         -- vim.api.nvim_create_autocmd({ "BufWritePre", "BufLeave", "WinLeave", "FocusLost" }, {
+    --         vim.api.nvim_create_autocmd({ "BufWritePre", "WinLeave", "FocusLost" }, {
+    --           buffer = bufnr,
+    --           command = "EslintFixAll",
+    --         })
+    --       end,
+    --       root_dir = vim.fs.dirname(vim.fs.find(".git", { path = startpath, upward = true })[1]),
+    --     },
+    --   },
+  },
 }
